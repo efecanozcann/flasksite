f218bceccb0d8270ad6dfc376430ab18ace89fba
Efe <efecanozcan452@gmail.com>
2026-03-29 23:06:03 +0300

    ilk yukleme

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+veriler = {
+    "promosyon": [],
+    "maddi": [],
+    "malzeme": []
+}
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        marka = request.form.get("marka")
+        tur = request.form.get("tur")
+
+        if not marka or not tur:
+            return "Tüm alanları doldur!"
+
+        veriler[tur].append(marka)
+        return redirect(url_for("panel"))
+
+    return render_template("index.html")
+
+
+@app.route("/panel")
+def panel():
+    return render_template("panel.html", veriler=veriler)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
